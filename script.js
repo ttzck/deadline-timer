5bf981287450bb4f8c45d9fe412790b6a5fb519d
ttzck <jnttzck@gmail.com>
2022-10-28 17:21:30 +0200

    style and calendar

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ console.log(urlParams);
 
 const start = new Date(urlParams.get("start"))
 const end = new Date(urlParams.get("end"))
-const now = new Date(Date.now());
+const now = new Date(Date.now())
 const duration = end - start
 const passed = now - start
 const left = end - now
+const daysPassed = Math.floor(passed / 1000.0 / 60.0 / 60.0 / 24.0)
+const daysLeft = Math.floor(left / 1000.0 / 60.0 / 60.0 / 24.0) + 1
 
 document.getElementById("startText").innerHTML = `the project started on ${start.toDateString()}`
 document.getElementById("endText").innerHTML = `the deadline is ${end.toDateString()}`
-document.getElementById("countdownText").innerHTML = `you have ${left} left of ${duration}`
+document.getElementById("countdownText").innerHTML = `${daysLeft} days`
+document.getElementById("calendar").innerHTML = "🟥".repeat(daysPassed) + "🟩".repeat(daysLeft)
+
