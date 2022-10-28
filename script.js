67b13f5b7a034c0f4ea14448d4bd92f280c0911f
ttzck <jnttzck@gmail.com>
2022-10-28 18:37:34 +0200

    bold text

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const daysLeft = Math.floor(left / 1000.0 / 60.0 / 60.0 / 24.0) + 1
 
 document.getElementById("startText").innerHTML = `the project started on ${start.toDateString()}`
 document.getElementById("endText").innerHTML = `the deadline is ${end.toDateString()}`
-document.getElementById("countdownText").innerHTML = `${daysLeft} days`
+document.getElementById("countdownText").innerHTML = `<b>${daysLeft}</b> days`
 document.getElementById("calendar").innerHTML 
 = Array(daysPassed).fill("■").join(" ")
 + " ▣ "
