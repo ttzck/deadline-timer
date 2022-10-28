944bce6579847158d81b814629d93d7a37fdc0d8
Jan <36882111+ttzck@users.noreply.github.com>
2022-10-28 18:21:41 +0200

    square calendar with current day

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ document.getElementById("startText").innerHTML = `the project started on ${start
 document.getElementById("endText").innerHTML = `the deadline is ${end.toDateString()}`
 document.getElementById("countdownText").innerHTML = `${daysLeft} days`
 document.getElementById("calendar").innerHTML 
-= "●".repeat(daysPassed) 
-+ "○".repeat(daysLeft)
+= "■".repeat(daysPassed) 
++ "▣"
++ "□".repeat(daysLeft - 1)
 
